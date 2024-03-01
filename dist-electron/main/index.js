57916229c40813e90d0424b6da772d82347d4896
qiongtubao <810776886@qq.com>
2024-03-02 03:19:19 +0800

    add vue module

---
+++ dist-electron/main/index.js
@@ -0,0 +1,85 @@
+import { app, BrowserWindow, ipcMain, shell } from "electron";
+import { release } from "node:os";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+globalThis.__filename = fileURLToPath(import.meta.url);
+globalThis.__dirname = dirname(__filename);
+process.env.DIST_ELECTRON = join(__dirname, "..");
+process.env.DIST = join(process.env.DIST_ELECTRON, "../dist");
+process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL ? join(process.env.DIST_ELECTRON, "../public") : process.env.DIST;
+if (release().startsWith("6.1"))
+  app.disableHardwareAcceleration();
+if (process.platform === "win32")
+  app.setAppUserModelId(app.getName());
+if (!app.requestSingleInstanceLock()) {
+  app.quit();
+  process.exit(0);
+}
+let win = null;
+const preload = join(__dirname, "../preload/index.mjs");
+const url = process.env.VITE_DEV_SERVER_URL;
+const indexHtml = join(process.env.DIST, "index.html");
+async function createWindow() {
+  win = new BrowserWindow({
+    title: "Main window",
+    icon: join(process.env.VITE_PUBLIC, "favicon.ico"),
+    webPreferences: {
+      preload
+      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+      // nodeIntegration: true,
+      // Consider using contextBridge.exposeInMainWorld
+      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+      // contextIsolation: false,
+    }
+  });
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(url);
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(indexHtml);
+  }
+  win.webContents.on("did-finish-load", () => {
+    win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
+  });
+  win.webContents.setWindowOpenHandler(({ url: url2 }) => {
+    if (url2.startsWith("https:"))
+      shell.openExternal(url2);
+    return { action: "deny" };
+  });
+}
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => {
+  win = null;
+  if (process.platform !== "darwin")
+    app.quit();
+});
+app.on("second-instance", () => {
+  if (win) {
+    if (win.isMinimized())
+      win.restore();
+    win.focus();
+  }
+});
+app.on("activate", () => {
+  const allWindows = BrowserWindow.getAllWindows();
+  if (allWindows.length) {
+    allWindows[0].focus();
+  } else {
+    createWindow();
+  }
+});
+ipcMain.handle("open-win", (_, arg) => {
+  const childWindow = new BrowserWindow({
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  if (process.env.VITE_DEV_SERVER_URL) {
+    childWindow.loadURL(`${url}#${arg}`);
+  } else {
+    childWindow.loadFile(indexHtml, { hash: arg });
+  }
+});
+//# sourceMappingURL=index.js.map
